4e6940cb76d675f24581277bfcc6d5e1290cb385
Julia França <francajulia663@gmail.com>
2026-04-23 19:32:27 -0300

    Add files via upload

---
+++ 01.javascript/testes.js
@@ -0,0 +1,21 @@
+// alert("Hello newyork!")
+// alert("SEGUNDA MENSAGEM!")
+
+let nome 
+
+nome = "Abiduzido"
+
+// alert("nome: " + nome)
+
+ let idade = 8
+//  alert("idade: " + idade)
+
+//  alert("nome:" + nome + "\nIdade:" + idade)
+
+//  nome = prompt("digiten seu nome: ") 
+//  alert("nome digitado: " + nome)
+
+
+let nota1
+nota1 = prompt("digite a sua nota: ")
+alert("nota digitada: " + nota1)
